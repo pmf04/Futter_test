1dbe72e6ba17da2cfc640aa50bc51a743b39de89
Myster PMF <mahbuburrahman0bd@gmail.com>
2024-01-29 13:49:42 +0600

    ok

---
+++ lib/pages/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Myster PMF",
+          style: TextStyle(
+            fontSize: 18,
+            color: Color.fromARGB(255, 184, 47, 13),
+            fontWeight: FontWeight.bold,
+            //blue background
+            backgroundColor: Color.fromARGB(255, 0, 0, 255),
+          ),
+        ),
+      ),
+      body: Container(
+        height: 400,
+        width: double.infinity,
+        color: Colors.cyan,
+        child: const Center(
+          child: Text(
+            "Hello World",
+            style: TextStyle(
+              fontSize: 24,
+              color: Color.fromARGB(255, 184, 47, 13),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
