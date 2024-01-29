bc681d9e08811bcdd250fbfcea2ec39d304aaf0c
pmf04 <102743163+pmf04@users.noreply.github.com>
2024-01-29 21:04:12 +0600

    asset added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -16,23 +17,46 @@ class Home extends StatelessWidget {
             //blue background
             backgroundColor: Color.fromARGB(255, 0, 0, 255),
           ),
-        ), 
+        ),
       ),
       body: Container(
         height: 400,
         width: double.infinity,
         color: Colors.cyan,
-        child: const Center(
-          child: Text(
-            "Hello World",
-            style: TextStyle(
-              fontSize: 24,
-              color: Color.fromARGB(255, 184, 47, 13),
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
+        child: Card(),
       ),
     );
+  }
+}
+
+class Card extends StatelessWidget {
+  const Card({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GFCard(
+        boxFit: BoxFit.cover,
+        image: Image.asset('images/flower.jpg'),
+        title: const GFListTile(
+          avatar: GFAvatar(
+            backgroundImage: AssetImage('images/flower.jpg'),
+          ),
+          title: Text('Myster PMF'),
+          subTitle: Text('Adventure lover'),
+        ),
+        content:
+            const Text("Myster PMF is a developer, programmer. He likes cats."),
+        buttonBar: GFButtonBar(
+          children: <Widget>[
+            GFButton(
+              onPressed: () {},
+              text: 'View Profile',
+            ),
+            GFButton(
+              onPressed: () {},
+              text: 'Go back',
+            ),
+          ],
+        ));
   }
 }
